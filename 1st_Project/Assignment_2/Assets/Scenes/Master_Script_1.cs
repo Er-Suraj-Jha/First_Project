57e349402f0c0ad24317cdba0d6efc7dd58a4001
Er-Suraj-Jha <61492858+Er-Suraj-Jha@users.noreply.github.com>
2020-03-19 12:31:46 +0530

    Added whole folder

---
+++ 1st_Project/Assignment_2/Assets/Scenes/Master_Script_1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Master_Script_1 : MonoBehaviour
+{
+  public Button button;
+  //public string newScene;
+  private void Start()
+  {
+    button.onClick.AddListener(OnButtonClick);
+  }
+
+  private void OnButtonClick()
+  {
+      Debug.Log("Button Clicked");
+      SceneManager.LoadScene("1st_Scene");
+  }
+ 
+    
+}
---
+++ 1st_Project/Assignment_2/Assets/Scenes/Second_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Second_Script : MonoBehaviour
+{
+  public Button button;
+  // public string newScene;
+  private void Start()
+  {
+    button.onClick.AddListener(OnButtonClick);
+  }
+
+  private void OnButtonClick()
+  {
+      Debug.Log("Button Clicked");
+      SceneManager.LoadScene("Master_Scene");
+  }
+ 
+    
+}
+
